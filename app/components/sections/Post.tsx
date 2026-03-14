825b5f25bf0d8630a7e47039b825d69617baea9a
malachyfernandez <jag_pyloric.0j@icloud.com>
2026-03-14 14:09:02 -0400

    comment field with arrow

--- app/components/sections/Post.tsx
+++ app/components/sections/Post.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import ContainerCol from '../layout/ContainerCol';
 import PoppinsText from '../ui/PoppinsText';
 import PoppinsTextInput from '../ui/PoppinsTextInput';
+import ContainerRow from '../layout/ContainerRow';
+import AppButton from '../ui/AppButton';
+import UpArrow from '../ui/UpArrow';
 
 interface PostProps {
     title: string;
@@ -13,21 +16,31 @@ interface PostProps {
 const Post = ({ title, description }: PostProps) => {
     // use state for text input
     const [comment, setComment] = useState('');
-    
+
     return (
-    <ContainerCol gap={1} className='p-4 bg-slate-800'>
-        <PoppinsText weight='bold'>{title}</PoppinsText>
-        <PoppinsText>{description}</PoppinsText>
-        <PoppinsTextInput
-            value={comment}
-            onChangeText={setComment}
-            placeholder="Add a comment..."
-        />
-    </ContainerCol>
+        <ContainerCol gap={1} className='p-4 bg-slate-800'>
+            <PoppinsText weight='bold'>{title}</PoppinsText>
+            <PoppinsText>{description}</PoppinsText>
+
+
+            <ContainerRow className='w-full justify-between'>
+
+                <PoppinsTextInput
+                    value={comment}
+                    onChangeText={setComment}
+                    placeholder="Add a comment..."
+                />
+                <AppButton variant='blue'>
+                    <UpArrow size={20} />
+                </AppButton>
+            </ContainerRow>
+            
+
+        </ContainerCol>
 
 
     );
-    
+
 };
 
 export default Post;
